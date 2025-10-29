b6f036e236a7d4ee4dfb078d4527aac503709a3a
Davide Minutoli <davideminutoli97@gmail.com>
2025-10-29 19:23:37 +0100

    corezzione ma template rotto ed innesistente

--- next.config.js
+++ next.config.js
@@ -11,6 +11,29 @@ const nextConfig = {
         hostname: '**'
       }
     ]
+  },
+  webpack: (config, { dev }) => {
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      'react-native$': 'react-native-web'
+    };
+    config.resolve.extensions = [
+      '.web.js',
+      '.web.jsx',
+      '.web.ts',
+      '.web.tsx',
+      ...config.resolve.extensions
+    ];
+
+    // Define __DEV__ for React Native
+    const webpack = require('webpack');
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        __DEV__: JSON.stringify(dev)
+      })
+    );
+
+    return config;
   }
 };
 
